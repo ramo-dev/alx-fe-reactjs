4d48bf578d9a3c49658c87dba84329abbea4d08c
ramo-dev <annuar.dev@gmail.com>
2024-08-05 21:25:58 +0300

    Added userprofile components which takes in props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   )
