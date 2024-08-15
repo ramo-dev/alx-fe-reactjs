26a7d8f480ff79a75079f21e321bd6c0aef4da3f
ramo-dev <annuar.dev@gmail.com>
2024-08-15 13:54:46 +0300

    replaced props with context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,13 @@
 import ProfilePage from './components/ProfilePage';
-import { userContext } from './components/UserContext';
+import { UserContext } from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <userContext.Provider value={userData} >
+    <UserContext.Provider value={userData} >
       <ProfilePage />
-    </userContext.Provider>
+    </UserContext.Provider>
   );
 }
 
