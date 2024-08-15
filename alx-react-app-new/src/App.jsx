e516d10fe6493c46385ba797573ad84af60e5197
ramo-dev <annuar.dev@gmail.com>
2024-08-15 12:19:38 +0300

    created counter component and some state to hold the count state and value

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <Footer />
     </>
   )
