2d7aa726fec350bee4ce7372585990818a9ecc32
ramo-dev <annuar.dev@gmail.com>
2024-08-24 15:06:52 +0300

    added, crud operations

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,20 +1,20 @@
+
+import { useNavigate } from 'react-router-dom';
 import useRecipeStore from "./recipeStore";
 
 const DeleteButton = ({ id }) => {
-
+  const navigate = useNavigate();
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
-
 
   function handleDelete() {
     deleteRecipe(id);
+    navigate('/');
   }
-
-
 
   return (
     <button className="delete" onClick={handleDelete}>Delete</button>
-  )
+  );
 }
 
-
 export default DeleteButton;
+
