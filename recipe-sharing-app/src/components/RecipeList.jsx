b53fd576663c5bc4afb1d9045bd4b6324270b5ed
ramo-dev <annuar.dev@gmail.com>
2024-08-24 15:45:19 +0300

    enhanced filtering and ui

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,30 +1,37 @@
-// RecipeList component
 import { Link } from "react-router-dom";
 import useRecipeStore from './recipeStore';
-
-
-
+import SearchBar from "./SearchBar";
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
-
+  const filtered = useRecipeStore(state => state.filteredRecipes);
 
   return (
-    <div >
-      {recipes.length > 0 ? (
-        recipes.map(recipe => (
-          <div key={recipe.id} className="recipes ">
+    <div>
+      <SearchBar />
+      {filtered.length > 0 ? (
+        filtered.map(recipe => (
+          <div key={recipe.id} className="recipes">
             <Link to={`/${recipe.id}`}>
               <h3>{recipe.title}</h3>
               <p>{recipe.description}</p>
             </Link>
           </div>
         ))
-      ) : <p>No recipes available</p>}
+      ) : recipes.length > 0 ? (
+        recipes.map(recipe => (
+          <div key={recipe.id} className="recipes">
+            <Link to={`/${recipe.id}`}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <p>No recipes available</p>
+      )}
     </div>
-  )
+  );
 };
 
-
 export default RecipeList;
-
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -15,7 +15,16 @@ const useRecipeStore = create(set => ({
       }
       return r;
     })
-  }))
+  })),
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })
+  ),
 }));
 
 export default useRecipeStore;
