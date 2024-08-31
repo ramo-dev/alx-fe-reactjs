996d7be6287b31e9fe9f8184b928533950bf0c55
ramo-dev <annuar.dev@gmail.com>
2024-08-31 20:16:03 +0300

    added caching,staletime, state configs

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,20 +2,33 @@ import { useQuery } from "react-query";
 
 const fetchData = async () => {
   const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!resp.ok) {
+    throw new Error('Network response was not ok');
+  }
   return resp.json();
 }
 
 const PostsComponent = () => {
 
-  const { data, isError, error, isLoading } = useQuery('fetchPosts', fetchData);
+  const { data, isError, error, isLoading } = useQuery('fetchPosts', fetchData, {
+
+    cacheTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 1,
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+  });
+
 
   if (isLoading) {
     return <div>Loading....</div>
   }
 
+
   if (isError) {
-    return <div>Error loading data : {error}</div>
+    return <div>Error loading data: {error.message}</div>
   }
+
+
   return (
     <div>
       {data.map((item) => (
@@ -24,10 +37,8 @@ const PostsComponent = () => {
           <p>{item.body}</p>
         </div>
       ))}
-
     </div>
-  )
+  );
 }
 
-
-export default PostsComponent
+export default PostsComponent;
