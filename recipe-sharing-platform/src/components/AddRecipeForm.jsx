0a37f6e195e7cb45bb5dbcc9145e3c776459c9aa
ramo-dev <annuar.dev@gmail.com>
2024-09-15 23:49:45 +0300

    styling the site using tailwind , added components for creating and sharing recipes

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -1,86 +1,125 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const AddRecipeForm = () => {
-  const [fields, setFields] = useState({
-    title: '',
-    summary: '',
-    ingredients: '',
-    preparation: ''
+  const [formData, setFormData] = useState({
+    title: "",
+    ingredients: "",
+    steps: "",
   });
 
-  const [errors, setErrors] = useState({})
+  const [errors, setErrors] = useState({
+    title: "",
+    ingredients: "",
+    steps: "",
+  });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFields({
-      ...fields, [name]: e.target.value
-    });
-  }
+    const { target } = e;
+    const name = target.name;
+    const value = target.value;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    if (!formData.title) {
+      setErrors((prev) => ({ ...prev, title: "Title is required" }));
+      isValid = false;
+    } else {
+      setErrors((prev) => ({ ...prev, title: "" }));
+    }
+
+    if (!formData.ingredients) {
+      setErrors((prev) => ({
+        ...prev,
+        ingredients: "Ingredient is required",
+      }));
+      isValid = false;
+    } else {
+      setErrors((prev) => ({ ...prev, ingredients: "" }));
+    }
+
+    if (!formData.steps) {
+      setErrors((prev) => ({
+        ...prev,
+        steps: "steps is required",
+      }));
+      isValid = false;
+    } else {
+      setErrors((prev) => ({ ...prev, steps: "" }));
+    }
+    return isValid;
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const validateFields = {}
-    if (!fields.title.trim()) {
-      validateFields.title = "Recipe title is required"
-    } if (!fields.summary.trim()) {
-      validateFields.summary = "Recipe summary is required"
-    }
-
-    if (!fields.ingredients.trim()) {
-      validateFields.ingredients = "Recipe ingredients are required"
+    e.preventDefault();
+    const isValid = validateForm();
+    if (!isValid) {
+      setFormData({ title: "", ingredients: "", steps: "" });
     } else {
-      const ingredientsArray = fields.ingredients.split(',')
-        .map(i => i.trim())
-        .filter(i => i !== "");
-
-      if (ingredientsArray.length < 3) {
-        validateFields.ingredients = "Recipe ingredients is below 3 items"
-      }
+      console.log(formData);
     }
-
-    if (!fields.preparation.trim()) {
-      validateFields.preparation = "Preparation steps are required"
-    }
-    setErrors(validateFields)
-    console.log('This errors:', validateFields);
-    console.log('Results:', fields);
-
-    if (Object.keys(validateFields).length === 0) {
-      alert('Form Sent');
-    }
-  }
+  };
 
   return (
-    <>
-      <h1 className='pl-8 mt-8 text-green-950'>ADD A NEW RECIPE</h1>
-      <form onSubmit={handleSubmit} className='font-medium flex flex-col gap-2 mx-6 mb-16 p-8 w-[400px] md:w-[600] border-solid border-blue-900 rounded shadow-gray-600'>
-        <label htmlFor="title">Recipe title</label>
-        <input className='border-solid border-2 border-slate-200 pl-2 leading-7' type="text" name='title' placeholder='Enter recipe name'
-          onChange={handleChange} />
-        {errors.title && <span className='text-red-600 text-xs'>{errors.title}</span>}
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col  sm:max-w-[450px] md:max-w-[550px] shadow p-6 text-left"
+    >
+      <div className="flex gap-4 p-4">
+        <label htmlFor="title" className="font-semibold text-base min-w-[90px]">
+          Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Add recipe"
+          onChange={handleChange}
+          value={formData.title}
+          className="border p-3"
+        />
+        {errors.title && <p> {errors.title}</p>}
+      </div>
+      <div className="flex gap-4 p-4">
+        <label
+          htmlFor="ingredients"
+          className="font-semibold text-base min-w-[90px]"
+        >
+          Ingredients
+        </label>
+        <textarea
+          name="ingredients"
+          id="ingredients"
+          placeholder="Write the ingredients"
+          onChange={handleChange}
+          value={formData.ingredients}
+          className="border p-3"
+        ></textarea>
+        {errors.ingredients && <p>{errors.ingredients}</p>}
+      </div>
+      <div className="flex gap-4 p-4">
+        <label htmlFor="steps" className="font-semibold text-base min-w-[90px]">
+          steps
+        </label>
+        <textarea
+          name="steps"
+          id="steps"
+          placeholder="How to prepare the recipe"
+          onChange={handleChange}
+          value={formData.steps}
+          className="border p-3"
+        ></textarea>
+        {errors.steps && <p>{errors.steps}</p>}
+      </div>
+      <button
+        type="submit"
+        className="bg-purple-500 rounded-full w-fit p-2 px-6 mx-auto hover:opacity-80"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
 
-        <label className='mt-4' htmlFor="summary"></label> Recipe summary
-        <input className='border-solid border-2 border-slate-200 pl-2 leading-7' type="text" name="summary" placeholder="Enter short description"
-          onChange={handleChange} />
-        {errors.summary && <span className='text-red-600 text-xs'>{errors.summary}</span>}
-
-        <label className='mt-4' htmlFor="recipeimage">Upload image</label>
-        <input className='' type="file" name="recipeimage" />
-
-        <label className='mt-4' htmlFor="ingredients">Recipe ingredients</label>
-        <textarea className='border-solid border-2 border-slate-200 pl-2 leading-7 ' name="ingredients" cols="40" rows="10"
-          onChange={handleChange} />
-        {errors.ingredients && <span className='text-red-600 text-xs'>{errors.ingredients}</span>}
-
-        <label className='mt-4' htmlFor="preparation">Recipe preparation</label>
-        <textarea className='border-solid border-2 border-slate-200 pl-2 leading-7' name="preparation" cols="40" rows="10"
-          onChange={handleChange} />
-        {errors.preparation && <span className='text-red-600 text-xs'>{errors.preparation}</span>}
-
-        <button className='bg-lime-800 py-2 mt-2 text-white mt-8' type="submit">Submit</button>
-      </form>
-    </>
-  )
-}
-
-export default AddRecipeForm
+export default AddRecipeForm;
